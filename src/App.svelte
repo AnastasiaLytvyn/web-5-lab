cc1e82c32fde883d1fd36b855bda78435173323a
Anastasiia Lytvyn <lytvynana@gmail.com>
2022-01-14 00:27:10 +0200

    fix by 3 lab

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
   import { todos, isAuthenticated, user, token } from "./store";
   import auth from "./authService";
   let auth0Client;
+  let userMsg = "";
 
   onMount(async () => {
     auth0Client = await auth.createClient();
@@ -24,16 +25,34 @@
     }
   });
   const addTodo = async () => {
+   
     const name = prompt("name") || "";
-    const { insert_todo_one } = await http.startExecuteMyMutation(
+    try {
+      userMsg = "Adding...";
+      const { insert_todo_one } = await http.startExecuteMyMutation(
       OperationDocsStore.addOne(name),
     );
     todos.update((n) => [...n, insert_todo_one]);
+     userMsg = null;
+    } catch (err) {
+      userMsg = `Error: ${err.message}`;
+      setTimeout(() => (userMsg = null), 5000);
+    }
+
   };
 
   const deleteTodo = async (id) => {
-    await http.startExecuteMyMutation(OperationDocsStore.deleteById(id));
-    todos.update((n) => n.filter((item) => item.id !== id));
+    try{
+      userMsg = "Deleting...";
+      await http.startExecuteMyMutation(OperationDocsStore.deleteById(id));
+      todos.update((n) => n.filter((item) => item.id !== id));
+      userMsg = null;
+    }
+    catch(err){
+      userMsg = `Error: ${err.message}`;
+      setTimeout(() => (userMsg = null), 5000);
+
+    }
   };
 
   function login() {
@@ -55,12 +74,13 @@
       <button on:click={logout}>Logout</button>
 
       <button on:click={addTodo}>Add new todo</button>
-
+      {#if userMsg}
+        <div>{userMsg}</div>
+      {/if}
       {#each $todos as todo}
         <div>
           <p>todo name: {todo.title}</p>
           <p>user id: {todo.user_id}</p>
-          <!-- delete todo by id -->
           <button on:click={() => deleteTodo(todo.id)}>Delete todo</button>
           <hr />
         </div>
@@ -72,8 +92,23 @@
 </main>
 
 <style>
+  :root {
+    --button-primary: #adb5bd;
+    --button-hover: #939aa1;
+  }
+
   main {
     margin: 0;
     padding: 0;
+  }
+
+  button {
+    cursor: pointer;
+    background-color: var(--button-primary);
+    transition: 0.3s;
+  }
+
+  button:hover {
+    background-color: var(--button-hover);
   }
 </style>
